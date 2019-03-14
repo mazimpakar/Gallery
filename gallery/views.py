f0b45f82b189dec718e0e82f8f8ad23a4ca75048
mazimpakar <rosemazimpaka2@gmail.com>
2019-03-14 16:02:03 +0200

    add templates file

--- gallery/views.py
+++ gallery/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from django.http  import HttpResponse
+from django.http  import HttpResponse,Http404
 import datetime as dt
 # Create your views here.
+# Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to the my  Gallery')
-
+    return render(request, 'welcome.html')
+    
 def gallery_of_day(request):
     date = dt.date.today()
     html = f'''
@@ -38,4 +39,22 @@ def gallery_of_day(request):
             </body>
         </html>
             '''
-    return HttpResponse(html)   
+    return HttpResponse(html)  
+def past_days_gallery(request,past_date):
+
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>Gallery for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)    
