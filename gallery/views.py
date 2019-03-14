d44a53185412d46cd259e09b47b0998388622ceb
mazimpakar <rosemazimpaka2@gmail.com>
2019-03-14 17:15:44 +0200

    add bootstrap for styling

--- gallery/views.py
+++ gallery/views.py
@@ -1,21 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http  import HttpResponse,Http404
 import datetime as dt
 # Create your views here.
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
-    
+
 def gallery_of_day(request):
     date = dt.date.today()
-    html = f'''
-        <html>
-            <body>
-                <h1> {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+    return render(request, 'all-gallery/today-gallery.html', {"date": date,})
 
 def convert_dates(dates):
 
@@ -27,34 +20,19 @@ def convert_dates(dates):
     # Returning the actual day of the week
     day = days[day_number]
     return day    
-def gallery_of_day(request):
-    date = dt.date.today()
 
-    # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>Gallery for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)  
-def past_days_gallery(request,past_date):
+def past_days_gallery(request, past_date):
 
     try:
         # Converts data from the string Url
-        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
 
     except ValueError:
         # Raise 404 error when ValueError is thrown
         raise Http404()
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>Gallery for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)    
+        assert False
+
+    if date == dt.date.today():
+        return redirect(gallery_of_day)
+
+    return render(request, 'all-gallery/past-gallery.html', {"date": date ,"gallery":gallery})
