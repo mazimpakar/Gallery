8fa9db3f6bf6588af40e4d47127d7735c0ec0c66
mazimpakar <rosemazimpaka2@gmail.com>
2019-03-15 11:02:39 +0200

    add navbar

--- gallery/urls.py
+++ gallery/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
-    url('^today/$',views.gallery_of_day,name='galleryToday'),
-    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_gallery,name = 'pastGallery') 
+    url('^location/$',views.location_of_gallery,name='locationGallery'),
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.category_gallery,name = 'categoryGallery') 
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -6,9 +6,10 @@ import datetime as dt
 def welcome(request):
     return render(request, 'welcome.html')
 
-def gallery_of_day(request):
+def location_of_gallery(request):
     date = dt.date.today()
-    return render(request, 'all-gallery/today-gallery.html', {"date": date,})
+    return render(request, 'all-gallery/location-gallery.html', {"date": date,})
+    
 
 def convert_dates(dates):
 
@@ -21,18 +22,18 @@ def convert_dates(dates):
     day = days[day_number]
     return day    
 
-def past_days_gallery(request, past_date):
+def category_gallery(request, past_date):
 
     try:
         # Converts data from the string Url
-        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+        date = dt.datetime.strptime(cotegory, '%Y-%m-%d').date()
 
     except ValueError:
         # Raise 404 error when ValueError is thrown
         raise Http404()
         assert False
 
-    if date == dt.date.today():
-        return redirect(gallery_of_day)
+    if date == dt.date.location():
+        return redirect(gallery_of_location)
 
-    return render(request, 'all-gallery/past-gallery.html', {"date": date ,"gallery":gallery})
+    return render(request, 'all-gallery/category-gallery.html', {"date": date ,"gallery":gallery})
