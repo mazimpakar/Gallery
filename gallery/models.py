4ce3aa6022a61ad50d5a80e9aff30adaa54d2740
mazimpakar <rosemazimpaka2@gmail.com>
2019-03-15 15:02:58 +0200

    add table in database

--- gallery/models.py
+++ gallery/models.py
@@ -1,38 +1,6 @@
 from django.db import models
 import datetime as dt
 # Create your models here.
-class Image(models.Model):
-    photo = models.ImageField(upload_to = 'images/')
-    image_name = models.CharField(max_length =30)
-    description = models.CharField(max_length =30)
-    location = models.Foreign Key(Location)
-    category = models.Foreign Key(Category)
-
-    def __str__(self):
-        return self.name
-
-    def save_image(self):
-        self.save()
-
-    def delete_image(self):
-        self.save()
-
-    
-    @classmethod
-    def get_image_by_id(cls, id):
-        image = Image.objects.get(id=id)
-        return image
-
-    @classmethod
-    def filter_by_location(cls, id):
-        image = Image.objects.filter(location_id=id).all()
-        return image
-
-    @classmethod
-    def get_images(cls):
-        images = Image.objects.all()
-        return images
-
 class Location(models.Model):
     """
     Class that contains location details of the image posted
@@ -63,6 +31,40 @@ class Category(models.Model):
 
     def del_cat(self):
         self.delete()
+
+class Image(models.Model):
+    photo = models.ImageField(upload_to = 'images/',blank=True,)
+    image_name = models.CharField(max_length =30)
+    description = models.CharField(max_length =30)
+    location = models.ForeignKey(Location,max_length=140,on_delete =models.CASCADE, blank=True, null=True) 
+    category = models.ForeignKey(Category,max_length=140,on_delete =models.CASCADE,  blank=True, null=True) 
+
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.save()
+
+    
+    @classmethod
+    def get_image_by_id(cls, id):
+        image = Image.objects.get(id=id)
+        return image
+
+    @classmethod
+    def filter_by_location(cls, id):
+        image = Image.objects.filter(location_id=id).all()
+        return image
+
+    @classmethod
+    def get_images(cls):
+        images = Image.objects.all()
+        return images
+
+
 
     
     
--- gallery/tests.py
+++ gallery/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import Image, Location, Category, 
+from .models import Image,Location,Category, 
 # Create your tests here.
 
 
@@ -72,74 +72,4 @@ class ImageTestClass(TestCase):
         self.image.save_image()
         images = Image.search_image('this')
         self.assertTrue(len(images)>0)
-class LocationTestClass(TestCase):
-    """
-    Tests Location class and its functions
-    """
-    #Set up method
-    def setUp(self):
-        self.locale = Location(name='here', description='testing pic')
-    #Testing instance
-    def test_instance(self):
-        self.assertTrue(isinstance(self.locale, Location))
-    
-    def test_save_method(self):
-        """
-        Function to test that location is being saved
-        """
-        self.locale.save_location()
-        locations = Location.objects.all()
-        self.assertTrue(len(locations) > 0)
-
-    def test_delete_method(self):
-        """
-        Function to test that a location can be deleted
-        """
-        self.locale.save_location()
-        self.locale.del_location()
-    
-    def test_update_method(self):
-        """
-        Function to test that a location's details can be updates
-        """
-        self.locale.save_location()
-        new_place = Location.objects.filter(name='here').update(name='hapa')
-        locations = Location.objects.get(name='hapa')
-        self.assertTrue(locations.name, 'hapa')
         
-class CategoryTestClass(TestCase):
-    """
-    Tests category class and its functions
-    """
-    #Set up method
-    def setUp(self):
-        self.cat = Category(name='this', description='testing pic')
-    #Testing instance
-    def test_instance(self):
-        self.assertTrue(isinstance(self.cat, Category))
-    
-    def test_save_method(self):
-        """
-        Function to test that category is being saved
-        """
-        self.cat.save_cat()
-        categories = Category.objects.all()
-        self.assertTrue(len(categories) > 0)
-
-    def test_delete_method(self):
-        """
-        Function to test that a category can be deleted
-        """
-        self.cat.save_cat()
-        self.cat.del_cat()
-    
-    def test_update_method(self):
-        """
-        Function to test that a category's details can be updates
-        """
-        self.cat.save_cat()
-        new_cat = Category.objects.filter(name='this').update(name='hii')
-        categories = Category.objects.get(name='hii')
-        self.assertTrue(categories.name, 'hii')
-
-      
