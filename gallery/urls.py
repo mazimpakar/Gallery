dc1c384996ce148d9ce639fb85504f8096a1d986
mazimpakar <rosemazimpaka2@gmail.com>
2019-03-15 16:29:50 +0200

    add photo file

--- gallery/urls.py
+++ gallery/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
-    url('^location/$',views.location_of_gallery,name='locationGallery'),
+    url(r'^location/$',views.location_of_gallery,name='locationGallery'),
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.category_gallery,name = 'categoryGallery') 
 ]
